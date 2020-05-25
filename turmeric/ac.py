420a726988bd741ffef10a6edca95c136aa65d5a
Cian ODonnell <cianodonnell12@gmail.com>
2020-05-25 20:10:08 +0100

    AC sim working for circuits with linear elements

--- turmeric/ac.py
+++ turmeric/ac.py
@@ -56,9 +56,9 @@ def ac_analysis(circ, start, points, stop, sweep_type=None,
         raise ValueError("ac_analysis(): AC analysis has number of points < 2 & start != stop")
     
     if sweep_type.upper() == options.ac_log_step or sweep_type is None:
-        fs = np.logspace(int(start), int(stop), int(points), endpoint=True)
+        fs = np.logspace(int(start), np.log(int(stop)), num=int(points), endpoint=True)
     elif sweep_type.upper() == options.ac_lin_step:
-        fs = np.linspace(int(start), int(stop), int(points), endpoint=True)
+        fs = np.linspace(int(start), int(stop), num=int(points), endpoint=True)
     else:
         raise ValueError(f"ac_analysis(): unknown sweep type {sweep_type}")
 
@@ -70,18 +70,18 @@ def ac_analysis(circ, start, points, stop, sweep_type=None,
     M0 = circ.M0
     ZAC0 = circ.ZAC0
     D0 = circ.D0
-    mats = [M0, ZAC0, D0]
-    keys = ['M0', 'ZAC0', 'D0']
-    for mat in zip(keys, mats):
-        print(mat)
-        
-    input()
+    #mats = [M0, ZAC0, D0]
+    #keys = ['M0', 'ZAC0', 'D0']
+    #for mat in zip(keys, mats):
+    #    print(mat)
+    #    
+    #input()
         
     # reduce the matrices
     M = M0[1:, 1:]
     ZAC = ZAC0[1:]
     D = D0[1:, 1:]
-    
+    ZAC[0,0] = 1
     if circ.is_nonlinear():
         logging.error("AC analysis does not currently support analysis of \
                       non-linear circuits")
@@ -91,7 +91,7 @@ def ac_analysis(circ, start, points, stop, sweep_type=None,
                               stype=sweep_type, op=x0, outfile=outfile)
 
     j = np.complex('j')
-    
+    print(fs)
     for f in fs:
         # evaluate the impedance at
         # the current frequency
@@ -107,6 +107,6 @@ def ac_analysis(circ, start, points, stop, sweep_type=None,
                 logging.error(f"ac_analysis(): singular matrix detected \
                               at {f} Hz")
                 raise ValueError
-    
+        sol.add_line(f, x)
    
     return sol
