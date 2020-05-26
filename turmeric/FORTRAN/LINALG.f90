251f813dfe045ea1c782a2d5a693b08fc3ccde2c
Cian ODonnell <cianodonnell12@gmail.com>
2020-05-26 11:42:25 +0100

    Added recursive determinant function

--- turmeric/FORTRAN/LINALG.f90
+++ turmeric/FORTRAN/LINALG.f90
@@ -360,7 +360,7 @@ REAL(8) FUNCTION pythag(A,B)
 
     REAL(8) :: absa, absb
     absa= abs(A)
-    absb=abs(A)
+    absb=abs(B)
     if(absa.gt.absb)then
         pythag=absa*sqrt(1.+(absb/absa)**2)
     else
@@ -372,5 +372,29 @@ REAL(8) FUNCTION pythag(A,B)
     endif
     return
     END FUNCTION pythag
+
+recursive function det(a, n, permanent) result(accumulation)
+  ! permanent -> 1 : compute permanent
+  ! permanent -> -1 : compute determinant
+  IMPLICIT NONE
+  real(8), intent(in) :: A(n, n)
+  integer, intent(in) :: n, permanent
+  real(8) :: b(n-1, n-1)
+  real(8) :: accumulation
+  integer :: i, sgn
+
+  if (n .eq. 1) then
+    accumulation = a(1, 1)
+  else
+    accumulation = 0
+    sgn = 1
+    do i=1, n
+      b(:, :(i-1)) = a(2:, :i-1)
+      b(:, i:) = a(2:, i+1:)
+      accumulation = accumulation + sgn * a(1,i) * det(b, n-1, permanent)
+      sgn = sgn * permanent
+    enddo
+  endif
+end function det
 
 end module LINALG
