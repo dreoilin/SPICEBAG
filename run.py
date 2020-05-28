38479a8a1c314366afced2d436d0c892172ba9d6
Cian ODonnell <cianodonnell12@gmail.com>
2020-05-28 15:36:33 +0100

    Full wave rectifier sim data added

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from turmeric import main
 
 #r = main('netlists/FifthOrderLowpass.net', outfile='tmp')
-r = main('netlists/TRAN/FW_RECT_SMOOTHING.net', outfile='tmp')
+r = main('netlists/TRAN/FW_RECT.net', outfile='tmp')
 
