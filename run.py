8e39b23201c05787af504d3a30eb0322955b72a4
Cian ODonnell <cianodonnell12@gmail.com>
2020-05-31 10:25:42 +0100

    Report deleted

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from turmeric import main
 
-#r = main('netlists/FifthOrderLowpass.net', outfile='tmp')
-r = main('netlists/TRAN/CLAMPER.net', outfile='tmp')
+r = main('netlists/AC/bw5lp.net', outprefix="tmp")
 
