fb18e34ac071dc0e8f5c032a0489f8da69df354c
Cian ODonnell <cianodonnell12@gmail.com>
2020-05-31 10:35:36 +0100

    Enhanced run file

--- run.py
+++ run.py
@@ -1,4 +1,30 @@
+"""
+Running turmeric as a python module
+"""
+
+# You can import the turmeric module using this command
 from turmeric import main
 
-r = main('netlists/AC/bw5lp.net', outprefix="tmp")
 
+# a simulation is run as follows:
+#   main(<netlist>, <ourprefix>)
+#   Our netlist syntax is described in the accompanying report
+results = main('netlists/AC/bw5lp.net', outprefix="tmp")
+
+# the results of a simulation (or simulations) are formatted as
+# a dictionary
+
+print("The voltage on node 5")
+#             <sim><variable>
+print(results['AC']['V(5)'])
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = np.absolute(results['AC']['f'])
+V = np.absolute(results['AC']['V(5)'])
+plt.semilogx(f, V)
+
+plt.title("Frequency response of a 5th Order LP Butterworth")
+plt.ylabel("Vout")
+plt.xlabel("f [Hz]")
